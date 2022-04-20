7aaf62049c9d9339cce1e5cf2a12a008489b36df
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-04-19 22:27:13 -0500

    v1

---
+++ api/Controller/actor.controller.js
@@ -0,0 +1,167 @@
+const mongoose = require("mongoose");
+const Actor = mongoose.model(process.env.ACTOR_MODEL);
+
+const SUCCESS_STATUS_CODE = process.env.SUCCESS_STATUS_CODE;
+const CREATED_STATUS_CODE = process.env.CREATED_STATUS_CODE;
+const NO_CONTENT_STATUS_CODE = process.env.NO_CONTENT_STATUS_CODE;
+const BAD_REQUEST_STATUS_CODE = process.env.BAD_REQUEST_STATUS_CODE;
+const NOT_FOUND_STATUS_CODE = process.env.NOT_FOUND_STATUS_CODE;
+const SERVER_ERROR_STATUS_CODE = process.env.SERVER_ERROR_STATUS_CODE;
+
+const NOT_FOUND_MSG = process.env.NOT_FOUND_MSG;
+const QUERYSTRING_SHOULD_BE_NUMBER_MSG = process.env.QUERYSTRING_SHOULD_BE_NUMBER_MSG;
+const CANNOT_EXCEED_COUNT_MSG = process.env.CANNOT_EXCEED_COUNT_MSG;
+
+
+// .then(()=>{
+
+// })
+// .catch(()=>{
+
+// })
+
+const getAllActors = (req, res) => {
+  let offset = parseInt(process.env.DEFAULT_FIND_OFFSET, 10);
+  let count = parseInt(process.env.DEFAULT_FIND_COUNT, 10);
+  const maxCount = parseInt(process.env.DEFAULT_MAX_FIND_LIMIT, 10);
+
+  if (req.query && req.query.offset) {
+    offset = parseInt(req.query.offset, 10);
+  }
+  if (req.query && req.query.count) {
+    offset = parseInt(req.query.count, 10);
+  }
+  if (isNaN(offset) || isNaN(count)) {
+    res.status(BAD_REQUEST_STATUS_CODE).json({ message: QUERYSTRING_SHOULD_BE_NUMBER_MSG });
+    return;
+  }
+  if (count > maxCount) {
+    res.status(BAD_REQUEST_STATUS_CODE).json({ message: CANNOT_EXCEED_COUNT_MSG, maxCount });
+    return;
+  }
+
+  Actor.find().skip(offset).limit(count).exec()
+  .then((actors)=>{
+    res.status(SUCCESS_STATUS_CODE).json(actors);
+  })
+  .catch((err)=>{
+    res.status(SERVER_ERROR_STATUS_CODE).json(err);
+  })
+};
+
+const getActor = (req, res) => {
+  const actorId = req.params.actorId;
+  Actor.findById(actorId).exec()
+  .then((actor)=>{
+    if(actor){
+      res.status(SUCCESS_STATUS_CODE).json(actor);
+    }
+    else {
+      res.status(NOT_FOUND_STATUS_CODE).json(NOT_FOUND_MSG);
+    }
+  })
+  .catch((err)=>{
+    res.status(SERVER_ERROR_STATUS_CODE).json(err);
+  })
+};
+
+const addActor = (req, res) => {
+  const newActor = {
+    name: req.body.name, year: req.body.year,
+    movies: req.body.movies
+  };
+  Actor.create(newActor)
+  .then((actor)=>{
+    res.status(CREATED_STATUS_CODE).json(actor);
+  })
+  .catch((err)=>{
+    res.status(SERVER_ERROR_STATUS_CODE).json(err);
+  })
+}
+
+
+const fullUpdateActor = (req, res) => {
+  actorUpdate = (req, res, actor, response) => {
+    actor.name = req.body.name;
+    actor.year = req.body.year;
+    actor.movies = req.body.movies;
+    actor.save()
+    .then((updatedActor)=>{
+      res.status(NO_CONTENT_STATUS_CODE).json(updatedActor);
+    })
+    .catch((err)=>{
+      res.status(SERVER_ERROR_STATUS_CODE).json(err);
+    })
+  }
+  _updateOne(req, res, actorUpdate);
+}
+
+const _updateOne = function (req, res, updateActorCallback) {
+  console.log("Update One actor Controller");
+  const actorId = req.params.actorId;
+  Actor.findById(actorId).exec(function (err, actor) {
+    const response = { status: 204, message: actor };
+    if (err) {
+      console.log("Error finding actor");
+      response.status = 500;
+      response.message = err;
+    } else if (!actor) {
+      console.log("actor id not found");
+      response.status = 404;
+      response.message = { "message": "actor ID not found" };
+    }
+    if (response.status !== 204) {
+      res.status(response.status).json(response.message);
+    } else {
+      updateActorCallback(req, res, actor, response);
+    }
+  });
+}
+
+// const partialUpdateActor = function (req, res) {
+//   console.log("Full Update One Actor Controller");
+//   actorUpdate = function (req, res, actor, response) {
+//     console.log(req.body.name);
+//     if (req.body.name) { actor.name = req.body.name; }
+//     if (req.body.year) { actor.year = req.body.year; }
+//     if (req.body.designers) { actor.designers = req.body.designers; }
+//     actor.save(function (err, updatedActor) {
+//       if (err) {
+//         response.status = 500;
+//         response.message = err;
+//       }
+//       res.status
+//         (response.status).json
+//         (response.message);
+//     });
+//   }
+//   _updateOne(req, res, actorUpdate);
+// }
+
+
+const deleteActor = (req, res) => {
+  const actorId = req.params.actorId;
+  Actor.findByIdAndDelete(actorId).exec((err, deletedActor) => {
+    const response = { status: 204 , message: deletedActor };
+    if (err) {
+      console.log("Error finding Actor");
+      response.status = 500;
+      response.message = err;
+    } else if (!deletedActor) {
+      console.log("Actor id not found");
+      response.status = 404;
+      response.message = {
+        "message": "Actor ID not found"
+      };
+    }
+    res.status(response.status).json(response.message);
+  });
+}
+module.exports = {
+  getAllActors,
+  getActor,
+  addActor,
+  deleteActor,
+  fullUpdateActor,
+  // partialUpdateActor
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+require("./api/data/db.js");
+const express = require("express");
+const routes = require("./api/routes");
+const app = express();
+
+app.set("port", process.env.PORT);
+
+
+// setup the express HttpRequest data parser middlewares
+// parse application/x-www-form-urlencoded (e.g. web form data)
+app.use(express.urlencoded({ extended: false }));
+// parse HttpRequest data to application/json format
+app.use(express.json());
+
+app.use("/api", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+app.use("/api", routes);
+
+const server = app.listen(app.get("port"), function () {
+  console.log(process.env.MSG_SERVER_START + server.address().port);
+});
